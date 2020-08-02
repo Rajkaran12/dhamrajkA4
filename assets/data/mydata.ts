a8723fd29c8b820760b7924fedfa6c245519c16d
Rajkaran12 <50668906+Rajkaran12@users.noreply.github.com>
2020-08-01 21:23:58 -0400

    Add files via upload

---
+++ assets/data/mydata.ts
@@ -0,0 +1,9 @@
+import { Dhamrajk } from '../../app/dhamrajk';
+
+export const MYINFO: Dhamrajk = {
+    snumber: 991542986,
+    name: 'Rajkaran Singh',
+    lname: 'dhamrajk',
+    cname: 'Davis',
+    atitle:'Assignment 4' 
+}
